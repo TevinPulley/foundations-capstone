494f7692faab3b4699ef72edbb71ee964d7c70eb
TevinPulley <tevinpulley@gmail.com>
2022-06-02 10:28:55 -0600

    deployment

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
+require("dotenv").config();
 const express = require("express");
+const path = require("express");
 const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
 
 const { getJobs, createJob, deleteJob } = require("./controller");
 
