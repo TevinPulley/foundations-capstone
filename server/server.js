836b42c988b15141204b446c3bc8c8454b042f30
TevinPulley <tevinpulley@gmail.com>
2022-06-02 20:02:05 -0600

    deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("express");
+const path = require("path");
 const cors = require("cors");
 const app = express();
 
